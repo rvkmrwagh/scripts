3115782feb8f9f9da52847e89a751731820c92e6
rvkmrwagh <82260861+rvkmrwagh@users.noreply.github.com>
2022-08-07 18:32:30 +0530

    Update pair_bluetooth_device.sh
    added commands to start power on bluetooth

--- pair_bluetooth_device.sh
+++ pair_bluetooth_device.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Script: pair_bluetooth_device.sh
 #About script: start bluetoot, filter bluetooth device and pair with it
-#Last_update: Aug 6 2022
+#Last_update: Aug 7 2022
 #######################################################################
 
 
@@ -11,23 +11,30 @@ function get_macaddress()
 	then
 		rm -rf tmp/bluetooth_scan_result
 	fi
+	
+	bluetoothctl power on
 	bluetoothctl --timeout 10 scan on >> /tmp/bluetooth_scan_result 
+	
 	if [[ $(grep -i $1 /tmp/bluetooth_scan_result | wc -l) -gt 0 ]]
 	then
 		mac_of_device=$(grep -i $1 /tmp/bluetooth_scan_result | tail -1 | awk '{print $3}')
 		echo "found device $1 has mac address $mac_of_device"
 		echo "pairing with $mac_of_device"
-		bluetoothctl connect $mac_of_device
 	else
 		echo "no bluetooth device found to pair matching to provided arguments : $1"
 		echo -e "\nhere is list of devices found \n"
 		cat /tmp/bluetooth_scan_result
+		exit
 	fi
+	bluetoothctl connect $mac_of_device
+
 }
 function start_bluetooth()
 {
 	if [[ $(systemctl is-active bluetooth | grep 'inactive' | wc -l) -eq 1 ]]
 	then
+		rfkill block bluetooth
+		rfkill unblock bluetooth
 		sudo systemctl start bluetooth
 		echo "waiting to start bluetooth service.."
 		sleep 10s
@@ -36,8 +43,9 @@ function start_bluetooth()
 			echo "Some issue in starting bluetooth"
 			return 1
 		fi
-		get_macaddress $1
 	fi
+	
+	get_macaddress $1
 }
 
 start_bluetooth $1
